a5b05de04a2effc7798113c697ae9849d4f96510
Fabio Valentini <decathorpe@gmail.com>
2022-06-15 17:05:17 +0200

    benches: test multiple packet sizes; fix logic for packet size > MTU

--- benches/wg_echo_client.py
+++ benches/wg_echo_client.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import pprint
 import signal
 import sys
 import textwrap
@@ -8,17 +9,17 @@ import timeit
 
 
 # generate 10000 unique test packets with the given length
-def gen_data(psize: int) -> list[bytes]:
+def gen_data(pnum: int, psize: int) -> list[bytes]:
     packets = []
 
-    for i in range(10000):
+    for i in range(pnum):
         packet = (f"{i:04d}".encode() * (psize // 4 + 1))[:psize]
         packets.append(packet)
 
     return packets
 
 
-async def main(bytes_out: list[bytes]):
+async def work(bytes_out: list[bytes]):
     r, w = await asyncio.open_connection("10.0.0.42", 1234)
 
     bytes_back = []
@@ -28,8 +29,15 @@ async def main(bytes_out: list[bytes]):
         w.write(packet)
         await w.drain()
 
-        read = await r.read(4096)
-        bytes_back.append(read)
+        recv_len = 0
+        recv_bytes = []
+
+        while recv_len != len(packet):
+            read = await r.read(4096)
+            recv_bytes.extend(read)
+            recv_len += len(read)
+
+        bytes_back.append(bytes(recv_bytes))
 
     w.close()
     await w.wait_closed()
@@ -41,16 +49,25 @@ async def main(bytes_out: list[bytes]):
         bytes_sent = sum(map(len, bytes_out))
         bytes_received = sum(map(len, bytes_back))
 
+        pprint.pprint(bytes_out)
+        pprint.pprint(bytes_back)
+
         print(f"Bytes Sent: {bytes_sent}")
         print(f"Bytes Received: {bytes_received}")
         print(f"Difference: {bytes_sent - bytes_received}")
 
+        raise
+
 
 if __name__ == "__main__":
-    sizes = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
+    numbs = [10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000]
+    sizes = [10, 20, 50, 100, 200, 500, 1000, 2000, 4096]
 
-    for size in sizes:
-        data = gen_data(size)
-        timer = timeit.Timer(lambda: asyncio.run(main(data), debug=True))
-        print(f"Packet size: {size} bytes")
-        print(timer.repeat(10, number=1))
+    for numb in numbs:
+        for size in sizes:
+            data = gen_data(numb, size)
+            timer = timeit.Timer(lambda: asyncio.run(work(data), debug=True))
+            print(f"Packet number: {numb}")
+            print(f"Packet size: {size} bytes")
+            print(timer.repeat(10, number=1))
+            print()
