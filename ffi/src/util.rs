586e7240bbd09a9b766e5a67cf3d2fe4999b7917
Maximilian Hils <git@maximilianhils.com>
2023-02-04 18:58:43 +0100

    base64 -> data-encoding (#20)

--- ffi/src/util.rs
+++ ffi/src/util.rs
@@ -1,3 +1,4 @@
+use data_encoding::BASE64;
 use pyo3::exceptions::PyOSError;
 use pyo3::types::{PyString, PyTuple};
 use pyo3::{exceptions::PyValueError, prelude::*};
@@ -11,7 +12,8 @@ pub fn string_to_key<T>(data: String) -> PyResult<T>
 where
     T: From<[u8; 32]>,
 {
-    base64::decode(data)
+    BASE64
+        .decode(data.as_bytes())
         .ok()
         .and_then(|bytes| <[u8; 32]>::try_from(bytes).ok())
         .map(T::from)
@@ -50,12 +52,12 @@ pub fn event_queue_unavailable<T>(_: mpsc::error::SendError<T>) -> PyErr {
 /// Generate a WireGuard private key, analogous to the `wg genkey` command.
 #[pyfunction]
 pub fn genkey() -> String {
-    base64::encode(StaticSecret::new(OsRng).to_bytes())
+    BASE64.encode(&StaticSecret::new(OsRng).to_bytes())
 }
 
 /// Derive a WireGuard public key from a private key, analogous to the `wg pubkey` command.
 #[pyfunction]
 pub fn pubkey(private_key: String) -> PyResult<String> {
     let private_key: StaticSecret = string_to_key(private_key)?;
-    Ok(base64::encode(PublicKey::from(&private_key).as_bytes()))
+    Ok(BASE64.encode(PublicKey::from(&private_key).as_bytes()))
 }
