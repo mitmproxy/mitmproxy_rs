3cba691b33e59d18d8994bb05835e6b0901f5c73
emanuele-em <micheletti.emanuele@hotmail.com>
2023-07-07 10:17:19 +0200

    function to create pipe programmatically

--- src/packet_sources/macos.rs
+++ src/packet_sources/macos.rs
@@ -51,12 +51,18 @@ pub fn deserialize_packet(buf: &[u8]) -> Result<MacosIpcRecv, prost::DecodeError
 
 pub struct PipeServer {
     ip_rx: pipe::Receiver,
+    ip_tx: pipe::Sender,
+    net_rx: pipe::Receiver,
+    net_tx: pipe::Sender,
+    filter_rx: pipe::Receiver,
+    filter_tx:pipe::Sender,
     ip_path: PathBuf,
     net_path: PathBuf,
+    filter_path: PathBuf,
 }
 
 impl PipeServer {
-    pub async fn new(ip_pipe: &str, net_pipe: &str) -> Result<Self> {
+    pub async fn new(ip_pipe: &str, net_pipe: &str, filter_pipe: &str) -> Result<Self> {
         let home_dir = match home_dir() {
             Some(ip_path) => ip_path,
             None => Err(anyhow!("Failed to get home directory"))?,
@@ -64,40 +70,38 @@ impl PipeServer {
 
         let ip_path = Path::new(&home_dir).join(format!("Downloads/{}.pipe", &ip_pipe));
         let net_path = Path::new(&home_dir).join(format!("Downloads/{}.pipe", &net_pipe));
+        let filter_path = Path::new(&home_dir).join(format!("Downloads/{}.pipe", &filter_pipe));
 
-        if !ip_path.exists() {
-            match mkfifo(&ip_path, Mode::S_IRWXU) {
-                Ok(_) => println!("created {:?}", ip_path),
+        let (ip_rx, ip_tx) = Self::create_pipe(&ip_path)?;
+        let (net_rx, net_tx) = Self::create_pipe(&net_path)?;
+        let (filter_rx, filter_tx) = Self::create_pipe(&filter_path)?;
+
+        Ok(PipeServer { ip_rx, ip_tx, net_rx, net_tx, filter_rx, filter_tx, ip_path, net_path, filter_path })
+    }
+
+    fn create_pipe(path: &PathBuf) -> Result<(pipe::Receiver, pipe::Sender)>{
+        if !path.exists() {
+            match mkfifo(path, Mode::S_IRWXU) {
+                Ok(_) => println!("created {:?}", path),
                 Err(e) => Err(anyhow!("Failed to create fifo: {:?}", e))?,
             }
         }
-
-        let ip_rx = match pipe::OpenOptions::new()
+        let rx = match pipe::OpenOptions::new()
             .unchecked(true)
-            .open_receiver(&ip_path)
+            .open_receiver(&path)
         {
-            Ok(ip_rx) => ip_rx,
+            Ok(rx) => rx,
             Err(e) => Err(anyhow!("Failed to open fifo receiver: {:?}", e))?,
         };
-        let _ip_tx = match pipe::OpenOptions::new()
+        let tx = match pipe::OpenOptions::new()
             .unchecked(true)
-            .open_sender(&ip_path)
+            .open_sender(&path)
         {
-            Ok(ip_rx) => ip_rx,
+            Ok(tx) => tx,
             Err(e) => Err(anyhow!("Failed to open fifo receiver: {:?}", e))?,
-        };
+       };
 
-
-        // if !net_path.exists() {
-        //     match mkfifo(&net_path, Mode::S_IRWXU) {
-        //         Ok(_) => println!("created net pipe {:?}", net_path),
-        //         Err(e) => Err(anyhow!("Failed to create net fifo: {:?}", e))?,
-        //     }
-        // }
-
-        // let _net_rx = pipe::OpenOptions::new().open_receiver(&net_path)?;
-
-        Ok(PipeServer { ip_rx, ip_path, net_path })
+        Ok((rx, tx))
     }
 }
 
@@ -134,7 +138,7 @@ impl PacketSourceConf for MacosConf {
     ) -> Result<(MacosTask, Self::Data)> {
         let executable_path = "/Applications/MitmproxyAppleTunnel.app/";
 
-        let ipc_server = match PipeServer::new("ip", "net").await {
+        let ipc_server = match PipeServer::new("ip", "net", "filter").await {
             Ok(server) => server,
             Err(e) => Err(anyhow!("Failed to create pipe server: {:?}", e))?,
         };
@@ -145,6 +149,7 @@ impl PacketSourceConf for MacosConf {
             .arg("--args")
             .arg(&ipc_server.ip_path)
             .arg(&ipc_server.net_path)
+            .arg(&ipc_server.filter_path)
             .arg(format!("{}", std::os::unix::process::parent_id()))
             .spawn();
 
@@ -243,31 +248,34 @@ impl PacketSourceTask for MacosTask {
                         NetworkCommand::SendPacket(packet) => {
                             let packet = serialize_packet(MacosIpcSend::Packet(packet.into_inner()));
 
-                            let net_tx = loop {
-                                match pipe::OpenOptions::new().open_sender(&self.ipc_server.net_path) {
-                                    Ok(net_tx) => break net_tx,
-                                    Err(e) if e.raw_os_error() == Some(6) => {},
-                                    Err(e) => return Err(e.into()),
+                            // let net_tx = loop {
+                            //     match pipe::OpenOptions::new().open_sender(&self.ipc_server.net_path) {
+                            //         Ok(net_tx) => break net_tx,
+                            //         Err(e) if e.raw_os_error() == Some(6) => {},
+                            //         Err(e) => return Err(e.into()),
+                            //     }
+                            //
+                            //     tokio::time::sleep(tokio::time::Duration::from_millis(50)).await;
+                            // };
+
+                            loop {
+                                    // Wait for the pipe to be writable
+                                    self.ipc_server.net_tx.writable().await?;
+
+                                    // Try to write data, this may still fail with `WouldBlock`
+                                    // if the readiness event is a false positive.
+                                    match self.ipc_server.net_tx.try_write(&packet) {
+                                        Ok(n) => {
+                                            break;
+                                        }
+                                        Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
+                                            continue;
+                                        }
+                                        Err(e) => {
+                                            return Err(e.into());
+                                        }
+                                    }
                                 }
-
-                                tokio::time::sleep(tokio::time::Duration::from_millis(50)).await;
-                            };
-
-                            // Wait for the pipe to be writable
-                            net_tx.writable().await?;
-                            // Try to write data, this may still fail with `WouldBlock`
-                            // if the readiness event is a false positive.
-                            match net_tx.try_write(&packet){
-                                Ok(n) => {
-                                    break;
-                                },
-                                Err(e) if e.kind() == std::io::ErrorKind::WouldBlock => {
-                                    continue;
-                                },
-                                Err(e) => {
-                                    return Err(e.into());
-                                },
-                            }
                             //self.ipc_server.tx.try_write(&self.buf[..len])?;
                         }
                     }
