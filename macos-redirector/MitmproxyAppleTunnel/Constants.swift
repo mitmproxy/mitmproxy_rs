3a063e8cca0e65634d34be7d88ebe21c2e8e0b2a
emanuele-em <micheletti.emanuele@hotmail.com>
2023-07-11 12:30:48 +0200

    macos redirector app + CI

---
+++ macos-redirector/MitmproxyAppleTunnel/Constants.swift
@@ -0,0 +1,5 @@
+struct K {
+    static let bundleIdentifier = "com.emanuele.MitmproxyAppleTunnel.MitmproxyAppleExtension"
+    static let serverAddress = "127.0.0.1"
+    static let port = "54345"
+}
