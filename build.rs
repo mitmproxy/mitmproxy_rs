0157ea1163eb99330bc2008b9324dbf438b25634
emanuele-em <micheletti.emanuele@hotmail.com>
2023-07-19 10:28:43 +0200

    idiomatic improvement

--- build.rs
+++ build.rs
@@ -2,10 +2,14 @@ extern crate prost_build;
 use protoc_bin_vendored;
 
 fn main() {
-    std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
-    prost_build::compile_protos(
-        &["./src/packet_sources/ipc.proto"],
-        &["./src/packet_sources/"],
-    )
-    .unwrap();
+    if let Ok(protoc_path) = protoc_bin_vendored::protoc_bin_path() {
+        std::env::set_var("PROTOC", protoc_path);
+        prost_build::compile_protos(
+            &["./src/packet_sources/ipc.proto"],
+            &["./src/packet_sources/"],
+        )
+        .unwrap();
+    } else {
+        panic!("Protoc path not found");
+    }
 }
