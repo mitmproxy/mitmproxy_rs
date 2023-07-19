f1b0bc8c3b486eec04fc0365fe17f6fe5720a6a0
emanuele-em <micheletti.emanuele@hotmail.com>
2023-07-19 10:36:19 +0200

    expect handling

--- build.rs
+++ build.rs
@@ -2,14 +2,10 @@ extern crate prost_build;
 use protoc_bin_vendored;
 
 fn main() {
-    if let Ok(protoc_path) = protoc_bin_vendored::protoc_bin_path() {
-        std::env::set_var("PROTOC", protoc_path);
-        prost_build::compile_protos(
-            &["./src/packet_sources/ipc.proto"],
-            &["./src/packet_sources/"],
-        )
-        .unwrap();
-    } else {
-        panic!("Protoc path not found");
-    }
+    std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().expect("protoc binary not found"));
+    prost_build::compile_protos(
+        &["./src/packet_sources/ipc.proto"],
+        &["./src/packet_sources/"],
+    )
+    .unwrap();
 }
