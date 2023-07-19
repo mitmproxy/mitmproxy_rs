09361eecb3d6c1b4a4407d8695d8e88c78213b5b
emanuele-em <micheletti.emanuele@hotmail.com>
2023-07-19 09:53:18 +0200

    protoc-bin-vendored

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 extern crate prost_build;
+use protoc_bin_vendored;
 
 fn main() {
+    std::env::set_var("PROTOC", protoc_bin_vendored::protoc_bin_path().unwrap());
     prost_build::compile_protos(
         &["./src/packet_sources/ipc.proto"],
         &["./src/packet_sources/"],
