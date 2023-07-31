189f98567d7d4d9023634150f0a09d9a9cd27549
emanuele-em <micheletti.emanuele@hotmail.com>
2023-07-31 11:02:14 +0200

    fix review hints, license

--- macos-certificate-truster/src/main.rs
+++ macos-certificate-truster/src/main.rs
@@ -1,26 +1,20 @@
-#[cfg(target_os = "macos")]
 use security_framework::{
     item::{ItemClass, ItemSearchOptions, Reference, SearchResult},
     trust_settings::{Domain, TrustSettings},
 };
 
 fn main() {
-    #[cfg(target_os = "macos")]
+    if let SearchResult::Ref(Reference::Certificate(cert)) = ItemSearchOptions::new()
+        .class(ItemClass::certificate())
+        .load_refs(true)
+        .label("mitmproxy")
+        .search()
+        .unwrap()
+        .first()
+        .unwrap()
     {
-        if let SearchResult::Ref(Reference::Certificate(cert)) = ItemSearchOptions::new()
-            .class(ItemClass::certificate())
-            .load_refs(true)
-            .label("mitmproxy")
-            .search()
-            .unwrap()
-            .first()
-            .unwrap()
-        {
-            TrustSettings::new(Domain::Admin)
-                .set_trust_settings_always(cert)
-                .unwrap();
-        }
+        TrustSettings::new(Domain::Admin)
+            .set_trust_settings_always(cert)
+            .unwrap();
     }
-    #[cfg(not(target_os = "macos"))]
-    println!("Certificate truster is only available on macos");
 }
