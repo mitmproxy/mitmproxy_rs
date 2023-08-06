7fff3033fe6e78e2e6c2e7ebbf58bda6865c1412
emanuele-em <micheletti.emanuele@hotmail.com>
2023-08-06 12:46:44 +0200

    issue #6302 - check protobuf at buildtime
    check if protoc already exists, otherwise include it with protoc_bin_vendored

--- build.rs
+++ build.rs
@@ -1,10 +1,17 @@
 extern crate prost_build;
 
 fn main() {
-    std::env::set_var(
-        "PROTOC",
-        protoc_bin_vendored::protoc_bin_path().expect("protoc binary not found"),
-    );
+    if std::env::var_os("PROTOC")
+        .map(std::path::PathBuf::from)
+        .or_else(|| which::which("protoc").ok())
+        .is_none()
+    {
+        std::env::set_var(
+            "PROTOC",
+            protoc_bin_vendored::protoc_bin_path().expect("Protoc binary not found"),
+        );
+    }
+
     prost_build::compile_protos(
         &["./src/packet_sources/ipc.proto"],
         &["./src/packet_sources/"],
