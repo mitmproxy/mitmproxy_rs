c4f851db43523f1505541afbe82285aaba048be9
emanuele-em <micheletti.emanuele@hotmail.com>
2023-08-12 12:20:11 +0200

    first we check for the presence of protoc locally

--- build.rs
+++ build.rs
@@ -1,8 +1,15 @@
 extern crate prost_build;
 
 fn main() {
-    if let Ok(protoc_path) = protoc_bin_vendored::protoc_bin_path() {
-        std::env::set_var("PROTOC", protoc_path);
+    if std::env::var_os("PROTOC")
+        .map(std::path::PathBuf::from)
+        .or_else(|| which::which("protoc").ok())
+        .is_none()
+    {
+        std::env::set_var(
+            "PROTOC",
+            protoc_bin_vendored::protoc_bin_path().expect("Protoc binary not found"),
+        );
     }
 
     prost_build::compile_protos(
