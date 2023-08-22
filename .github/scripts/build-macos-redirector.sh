08e0656018593772bdd45f205f4f2f7b129707ea
Maximilian Hils <git@maximilianhils.com>
2023-08-22 14:03:38 +0200

    ci++

--- .github/scripts/build-macos-redirector.sh
+++ .github/scripts/build-macos-redirector.sh
@@ -12,7 +12,7 @@ if [ -n "$APPLE_ID" ]; then
   echo -n "$APPLE_PROVISIONING_PROFILE_EXT" | base64 --decode -o "~/Library/MobileDevice/Provisioning Profiles/474ba41d-1dac-40c2-88a5-4ab7266108c7.provisionprofile"
 
 
-  security import -A <(echo -n "$APPLE_CERTIFICATE" | base64 --decode)
+  security import <(echo -n "$APPLE_CERTIFICATE" | base64 --decode) -A
 
 #  echo -n "$APPLE_CERTIFICATE" | base64 --decode -o  "$RUNNER_TEMP/build.cer"
 #  ls -l "$RUNNER_TEMP"
