f16efe4ac48b6b5a9038e6f7b5e2e2675710e732
Maximilian Hils <git@maximilianhils.com>
2023-08-22 13:33:55 +0200

    ci++

--- .github/scripts/build-macos-redirector.sh
+++ .github/scripts/build-macos-redirector.sh
@@ -10,10 +10,7 @@ if [ -n "$APPLE_ID" ]; then
   # from https://developer.apple.com/account/resources/profiles/list, base64'd
   echo -n "$APPLE_PROVISIONING_PROFILE_APP" | base64 --decode -o "~/Library/MobileDevice/Provisioning Profiles/99970b7c-e88e-44b5-b44a-e0eabf3c291f.provisionprofile"
   echo -n "$APPLE_PROVISIONING_PROFILE_EXT" | base64 --decode -o "~/Library/MobileDevice/Provisioning Profiles/474ba41d-1dac-40c2-88a5-4ab7266108c7.provisionprofile"
-
-  echo -n "$APPLE_PROVISIONING_PROFILE_APP" | base64 --decode -o "/Library/MobileDevice/Provisioning Profiles/99970b7c-e88e-44b5-b44a-e0eabf3c291f.provisionprofile"
-  echo -n "$APPLE_PROVISIONING_PROFILE_EXT" | base64 --decode -o "/Library/MobileDevice/Provisioning Profiles/474ba41d-1dac-40c2-88a5-4ab7266108c7.provisionprofile"
-
+  security import <(echo -n "$APPLE_CERTIFICATE" | base64 --decode)
 
   ls -l "~/Library/MobileDevice/Provisioning Profiles/"
 
