95975295da391317e9129f834a03fb9fd233736c
Maximilian Hils <git@maximilianhils.com>
2023-09-10 01:07:00 +0200

    pathname is a thing now

--- mitmproxy-macos/redirector/network-extension/TransparentProxyProvider.swift
+++ mitmproxy-macos/redirector/network-extension/TransparentProxyProvider.swift
@@ -1,4 +1,7 @@
+import Darwin
 import NetworkExtension
+import Foundation
+
 
 class TransparentProxyProvider: NETransparentProxyProvider {
     
@@ -39,6 +42,49 @@ class TransparentProxyProvider: NETransparentProxyProvider {
     
     override func handleNewFlow(_ flow: NEAppProxyFlow) -> Bool {
         log.debug("handleNewFlow")
-        return true
+        let meta = flow.metaData;
+        log.debug("meta \(String(describing: meta), privacy: .public)")
+        log.debug("flow \(String(describing: flow), privacy: .public)")
+        log.debug("sourceAppSigningIdentifier \(String(describing: meta.sourceAppSigningIdentifier), privacy: .public)")
+        log.debug("sourceAppUniqueIdentifier \(String(describing: meta.sourceAppUniqueIdentifier), privacy: .public)")
+
+        log.debug("sourceAppAuditToken \(String(describing: meta.sourceAppAuditToken!), privacy: .public)")
+
+        guard let auditToken = flow.metaData.sourceAppAuditToken else {
+              return false
+        }
+
+        guard auditToken.count == MemoryLayout<audit_token_t>.size else {
+            return false
+        }
+
+        let tokenT: audit_token_t? = auditToken.withUnsafeBytes { buf in
+          guard let baseAddress = buf.baseAddress else {
+            return nil
+          }
+          return baseAddress.assumingMemoryBound(to: audit_token_t.self).pointee
+        }
+
+        guard let token = tokenT else {
+          return false
+        }
+
+        let pid = audit_token_to_pid(token)
+        
+        log.debug("pid \(pid)")
+        
+        let PROC_PIDPATHINFO_MAXSIZE = MAXPATHLEN * 4
+    
+        let pathBuffer = UnsafeMutablePointer<UInt8>.allocate(capacity: Int(PROC_PIDPATHINFO_MAXSIZE))
+        defer {
+            pathBuffer.deallocate()
+        }
+        let pathLength = proc_pidpath(pid, pathBuffer, UInt32(PROC_PIDPATHINFO_MAXSIZE))
+        if pathLength > 0 {
+            let path = String(cString: pathBuffer)
+            log.debug("  path=\(path, privacy: .public)")
+        }
+    
+        return false
     }
 }
