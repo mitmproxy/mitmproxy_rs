c435b92f9c84a06d020529d8d756996312dff8e6
Maximilian Hils <git@maximilianhils.com>
2023-09-21 12:50:09 +0200

    declare pyinstaller hidden imports

--- mitmproxy-rs/mitmproxy_rs/_pyinstaller/hook-mitmproxy_rs.py
+++ mitmproxy-rs/mitmproxy_rs/_pyinstaller/hook-mitmproxy_rs.py
@@ -1,3 +1,11 @@
+import sys
 from PyInstaller.utils.hooks import collect_data_files
 
 datas = collect_data_files("mitmproxy_rs")
+
+hiddenimports = []
+
+if sys.platform == "darwin":
+    hiddenimports.append("mitmproxy_macos")
+elif sys.platform == "win32":
+    hiddenimports.append("mitmproxy_windows")
