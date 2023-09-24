341e3006ee7858c4fe5c80d26169c65de399ba94
Maximilian Hils <git@maximilianhils.com>
2023-09-24 19:41:48 +0200

    turn `mitmproxy_windows.executable_path` into a method
    now that this is auto-imported to check for missing packages, we don't want dev compiles on every run

--- mitmproxy-windows/mitmproxy_windows/__init__.py
+++ mitmproxy-windows/mitmproxy_windows/__init__.py
@@ -1,24 +1,34 @@
 from pathlib import Path
 
-here = Path(__file__).parent.absolute()
-executable_path = here / "windows-redirector.exe"
 
-# Development path: This should never happen with precompiled wheels.
-if not executable_path.exists() and (here / "editable.marker").exists():
-    import logging
-    import shutil
-    import subprocess
+def executable_path() -> Path:
+    """
+    Return the Path for windows-redirector.exe.
 
-    logger = logging.getLogger(__name__)
-    logger.warning("Development mode: Compiling windows-redirector.exe...")
+    For shipped wheels this is just the file in the package,
+    for development setups this may invoke cargo to build it.
+    """
+    here = Path(__file__).parent.absolute()
+    exe = here / "windows-redirector.exe"
 
-    # Build Redirector
-    subprocess.run(["cargo", "build"], cwd=here.parent / "redirector", check=True)
-    # Copy WinDivert to target/debug/
-    target_debug = here.parent.parent / "target/debug"
-    for f in ["WinDivert.dll", "WinDivert.lib", "WinDivert64.sys"]:
-        if not (target_debug / f).exists():
-            shutil.copy(here / f, target_debug / f)
+    # Development path: This should never happen with precompiled wheels.
+    if not exe.exists() and (here / "editable.marker").exists():
+        import logging
+        import shutil
+        import subprocess
 
-    logger.warning("Development mode: Using target/debug/windows-redirector.exe...")
-    executable_path = target_debug / "windows-redirector.exe"
+        logger = logging.getLogger(__name__)
+        logger.warning("Development mode: Compiling windows-redirector.exe...")
+
+        # Build Redirector
+        subprocess.run(["cargo", "build"], cwd=here.parent / "redirector", check=True)
+        # Copy WinDivert to target/debug/
+        target_debug = here.parent.parent / "target/debug"
+        for f in ["WinDivert.dll", "WinDivert.lib", "WinDivert64.sys"]:
+            if not (target_debug / f).exists():
+                shutil.copy(here / f, target_debug / f)
+
+        logger.warning("Development mode: Using target/debug/windows-redirector.exe...")
+        exe = target_debug / "windows-redirector.exe"
+
+    return exe
