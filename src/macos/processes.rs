54034d83a0a7450ecdf45dbe7934bf8bc291e080
emanuele-em <micheletti.emanuele@hotmail.com>
2023-10-23 20:25:41 +0200

    test with objc and cocoa

--- src/macos/processes.rs
+++ src/macos/processes.rs
@@ -1,13 +1,15 @@
 use anyhow::Result;
-use objc2::runtime::NSObject;
-use objc2::runtime::NSObjectProtocol;
-use objc2::ffi::id;
-use objc2::msg_send;
-use objc2::class;
+//use objc::runtime::AnyClass;
 use sysinfo::{PidExt, ProcessRefreshKind, System, SystemExt};
 use crate::processes::ProcessList;
-use objc2::{extern_class, msg_send_id, mutability, ClassType};
-use icrate::AppKit::NSRunningApplication;
+
+use cocoa::appkit::NSRunningApplication;
+
+use cocoa::{
+    base::{id, nil},
+};
+use objc::{sel, msg_send, class};
+
 // extern_class!(
 //     #[derive(Debug, PartialEq, Eq, Hash)]
 //     #[cfg(feature = "AppKit_NSRunningApplication")]
@@ -47,15 +49,31 @@ pub fn active_executables() -> Result<ProcessList> {
     sys.refresh_processes_specifics(ProcessRefreshKind::new());
     for proc in sys.processes() {
         dbg!(proc);
-
-        let int_value = proc.0.as_u32();
+        //dbg!(AnyClass::classes_count());
+        pub type pid_t = i32;
+        let int_value: pid_t = proc.0.as_u32() as i32;
         // let cls = NSRunningApplication::class();
+        //let nsrunning = class!(NSRunningApplication);
         unsafe{
-            let p: i32 = msg_send![
-                class!(NSRunningApplication),
-                runningApplicationWithProcessIdentifier: int_value
-            ];
-        dbg!(p);
+            //let current_app = NSRunningApplication::runningApplicationWithProcessIdentifier(nil, int_value);
+            let app_class = class!(NSRunningApplication);
+            let app: id = msg_send![app_class, currentApplication];
+            let app_name: id = msg_send![app, localizedName];
+            //let app_name: id = msg_send![current_app, localizedName];
+            dbg!(app_name);
+            //current_app.activateWithOptions_(NSApplicationActivateIgnoringOtherApps);
+            // let superclass = class!(NSObject);
+            // let mut decl = objc2::declare::ClassBuilder::new("NSRunningApplication", superclass).unwrap();
+            // decl.register();
+            //dbg!(AnyClass::get("NSRunningApplication"));
+            // let app_class = class!(NSRunningApplication);
+            // let app_with_process_id: *mut Object = msg_send![
+            //     app_class,
+            //     runningApplicationWithProcessIdentifier: process_id as i64
+            // ];
+            //let app_class = class!(NSRunningApplication);
+            //let p: i32 = msg_send![ app_class, runningApplicationWithProcessIdentifier: int_value ];
+            //dbg!(p);
         };
 
     }
