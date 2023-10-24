96e56cd3a1048ad227089d24c4801521d0a02d49
emanuele-em <micheletti.emanuele@hotmail.com>
2023-10-24 20:30:46 +0200

    tst

--- src/macos/processes.rs
+++ src/macos/processes.rs
@@ -3,13 +3,13 @@ use anyhow::Result;
 use sysinfo::{PidExt, ProcessRefreshKind, System, SystemExt};
 use crate::processes::ProcessList;
 
-use cocoa::appkit::NSRunningApplication;
+//use cocoa::appkit::NSRunningApplication;
 
-use cocoa::{
-    base::{id, nil},
-};
-use objc::{sel, msg_send, class};
-
+// use cacao::foundation::{id, nil, NSString,  NSURL, BOOL};
+// use objc::{sel, sel_impl, msg_send, class};
+use std::ptr;
+use std::os::raw::{c_char, c_int};
+use libc::{proc_pidpath, proc_name,PROC_PIDPATHINFO_MAXSIZE, c_void, PROC_PIDTBSDINFO, proc_pidinfo, proc_bsdinfo};
 // extern_class!(
 //     #[derive(Debug, PartialEq, Eq, Hash)]
 //     #[cfg(feature = "AppKit_NSRunningApplication")]
@@ -44,23 +44,70 @@ use objc::{sel, msg_send, class};
 
 pub fn active_executables() -> Result<ProcessList> {
     dbg!("active_executables");
-
     let mut sys = System::new();
     sys.refresh_processes_specifics(ProcessRefreshKind::new());
     for proc in sys.processes() {
-        dbg!(proc);
+        // dbg!(proc);
         //dbg!(AnyClass::classes_count());
-        pub type pid_t = i32;
-        let int_value: pid_t = proc.0.as_u32() as i32;
+        //let int_value: pid_t = proc.0.as_u32() as i32;
         // let cls = NSRunningApplication::class();
         //let nsrunning = class!(NSRunningApplication);
-        unsafe{
+        unsafe {
+                // let mut path_buffer: [c_char; PROC_PIDPATHINFO_MAXSIZE as usize] = [0; PROC_PIDPATHINFO_MAXSIZE as usize];
+                // let mut name_buffer: [c_char; 256] = [0; 256];
+                const PROC_PIDTBSDINFO_SIZE:usize = std::mem::size_of::<proc_bsdinfo>();
+                let mut bsd_info: [c_char;  PROC_PIDTBSDINFO_SIZE] = [0; PROC_PIDTBSDINFO_SIZE];
+
+                let ret = proc_pidinfo(proc.0.as_u32() as i32, PROC_PIDTBSDINFO, 0, bsd_info.as_mut_ptr() as *mut c_void, PROC_PIDTBSDINFO_SIZE as i32);
+                let bsd_info_struct: proc_bsdinfo = std::mem::transmute(bsd_info);
+                if ret as usize == PROC_PIDTBSDINFO_SIZE {
+                    println!("Process ID: {}", bsd_info_struct.pbi_pid);
+                    println!("Process Name: {:?}", std::ffi::CStr::from_ptr(bsd_info_struct.pbi_comm.as_ptr()));
+                    println!("Process status: {:?}", bsd_info_struct.pbi_status);
+                } else {
+                    println!("VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV");
+                    println!("Error for pid {}", proc.0.as_u32());
+                    println!("Process ID: {}", bsd_info_struct.pbi_pid);
+                    println!("Process Name: {:?}", std::ffi::CStr::from_ptr(bsd_info_struct.pbi_comm.as_ptr()));
+                    println!("Process status: {:?}", bsd_info_struct.pbi_status);
+                    println!("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^");
+                }
+
+                // proc_pidpath(proc.0.as_u32() as i32, path_buffer.as_mut_ptr() as *mut c_void, PROC_PIDPATHINFO_MAXSIZE as u32);
+                // proc_name(proc.0.as_u32() as i32, name_buffer.as_mut_ptr() as *mut c_void, size_of_val);
+
+                // let path_str = std::ffi::CStr::from_ptr(path_buffer.as_ptr()).to_str().unwrap();
+                // let name_str = std::ffi::CStr::from_ptr(name_buffer.as_ptr()).to_str().unwrap();
+
+                // println!("Path: {}\nName: {}", path_str, name_str);
+                //let proces_info = class!(NSPRocessInfo);
+                //let app: id = msg_send![app_class, currentApplication];
+                // let app: id = msg_send![app_class, runningApplicationWithProcessIdentifier: proc.0.as_u32() as i32];
+                // let localized_name: id = msg_send![app, localizedName];
+                // if localized_name != nil {
+                //     let localized_name = NSString::retain(localized_name).to_string();
+                //     dbg!(localized_name);
+                // }
+                //
+                // let bundle_identifier: id = msg_send![app, bundleIdentifier];
+                // if bundle_identifier != nil {
+                //     let bundle_identifier = NSString::retain(bundle_identifier).to_string();
+                //     // dbg!(bundle_identifier);
+                //
+                //     let hidden: BOOL = msg_send![app, isHidden];
+                //     // dbg!(hidden);
+                // }
+                //
+                // let executable: id = msg_send![app, executableURL];
+                // if executable != nil {
+                //     let executable = NSURL::retain(executable).pathbuf();
+                //     // dbg!(executable);
+                // }
+
+        }
+        // unsafe{
             //let current_app = NSRunningApplication::runningApplicationWithProcessIdentifier(nil, int_value);
-            let app_class = class!(NSRunningApplication);
-            let app: id = msg_send![app_class, currentApplication];
-            let app_name: id = msg_send![app, localizedName];
             //let app_name: id = msg_send![current_app, localizedName];
-            dbg!(app_name);
             //current_app.activateWithOptions_(NSApplicationActivateIgnoringOtherApps);
             // let superclass = class!(NSObject);
             // let mut decl = objc2::declare::ClassBuilder::new("NSRunningApplication", superclass).unwrap();
@@ -74,7 +121,7 @@ pub fn active_executables() -> Result<ProcessList> {
             //let app_class = class!(NSRunningApplication);
             //let p: i32 = msg_send![ app_class, runningApplicationWithProcessIdentifier: int_value ];
             //dbg!(p);
-        };
+        // };
 
     }
 
