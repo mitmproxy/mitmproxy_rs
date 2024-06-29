7cbaad033846f7edac2a2d42fd00656560b4a5cc
Gaurav Jain <gjain1563@gmail.com>
2024-06-30 02:34:35 +0530

    Restructure getaddrinfo

--- mitmproxy-rs/src/dns_resolver.rs
+++ mitmproxy-rs/src/dns_resolver.rs
@@ -1,22 +1,19 @@
-use hickory_resolver::config::*;
-use hickory_resolver::proto::rr::RecordType;
+use hickory_resolver::config::{LookupIpStrategy, ResolverConfig, ResolverOpts};
 use hickory_resolver::TokioAsyncResolver;
-use pyo3::exceptions::PyValueError;
-use pyo3::prelude::*;
+use pyo3::types::PyAny;
+use pyo3::prelude::{pyfunction, PyResult, Python};
 
 #[pyfunction]
-#[pyo3(signature = (host, record_type))]
-pub fn getaddrinfo(py: Python<'_>, host: String, record_type: u32) -> PyResult<&PyAny> {
+#[pyo3(signature = (host, is_ipv6))]
+pub fn getaddrinfo(py: Python<'_>, host: String, is_ipv6: bool) -> PyResult<&PyAny> {
     pyo3_asyncio::tokio::future_into_py(py, async move {
-        let record_type = match record_type {
-            1 => RecordType::A,
-            28 => RecordType::AAAA,
-            // TODO: Add more record types
-            _ => return Err(PyValueError::new_err("Unsupported record type")),
-        };
+        let mut opts = ResolverOpts::default();
+        if is_ipv6 {
+            opts.ip_strategy = LookupIpStrategy::Ipv6Only;
+        }
         let resolver =
-            TokioAsyncResolver::tokio(ResolverConfig::default(), ResolverOpts::default());
-        let response = resolver.lookup(host, record_type).await.unwrap();
+            TokioAsyncResolver::tokio(ResolverConfig::default(), opts);
+        let response = resolver.lookup_ip(host).await.unwrap();
         let addresses: Vec<String> = response.iter().map(|addr| addr.to_string()).collect();
         Ok(addresses)
     })
