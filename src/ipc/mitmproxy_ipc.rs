02ebdf0eab64be6be8c3ad2687c80aa5c306951f
Maximilian Hils <git@maximilianhils.com>
2024-09-01 22:07:29 +0200

    Windows: make `executable_icon` raise if the file is not found. (#170)
    * windows: make `executable_icon` raise if the file is not found.
    
    * [autofix.ci] apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- src/ipc/mitmproxy_ipc.rs
+++ src/ipc/mitmproxy_ipc.rs
@@ -6,7 +6,6 @@
 // or file a PR and let CI handle it.
 
 /// Packet with associated tunnel info (Windows pipe to mitmproxy)
-#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct PacketWithMeta {
     #[prost(bytes = "vec", tag = "1")]
@@ -14,7 +13,6 @@ pub struct PacketWithMeta {
     #[prost(message, optional, tag = "2")]
     pub tunnel_info: ::core::option::Option<TunnelInfo>,
 }
-#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct TunnelInfo {
     #[prost(uint32, tag = "1")]
@@ -23,7 +21,6 @@ pub struct TunnelInfo {
     pub process_name: ::core::option::Option<::prost::alloc::string::String>,
 }
 /// Packet or intercept spec (Windows pipe to redirector)
-#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct FromProxy {
     #[prost(oneof = "from_proxy::Message", tags = "1, 2")]
@@ -31,7 +28,6 @@ pub struct FromProxy {
 }
 /// Nested message and enum types in `FromProxy`.
 pub mod from_proxy {
-    #[allow(clippy::derive_partial_eq_without_eq)]
     #[derive(Clone, PartialEq, ::prost::Oneof)]
     pub enum Message {
         #[prost(message, tag = "1")]
@@ -41,14 +37,12 @@ pub mod from_proxy {
     }
 }
 /// Packet (macOS UDP Stream)
-#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Packet {
     #[prost(bytes = "vec", tag = "1")]
     pub data: ::prost::alloc::vec::Vec<u8>,
 }
 /// Intercept conf (macOS Control Stream)
-#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct InterceptConf {
     #[prost(bool, tag = "1")]
@@ -57,7 +51,6 @@ pub struct InterceptConf {
     pub actions: ::prost::alloc::vec::Vec<::prost::alloc::string::String>,
 }
 /// New flow (macOS TCP/UDP Stream)
-#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct NewFlow {
     #[prost(oneof = "new_flow::Message", tags = "1, 2")]
@@ -65,7 +58,6 @@ pub struct NewFlow {
 }
 /// Nested message and enum types in `NewFlow`.
 pub mod new_flow {
-    #[allow(clippy::derive_partial_eq_without_eq)]
     #[derive(Clone, PartialEq, ::prost::Oneof)]
     pub enum Message {
         #[prost(message, tag = "1")]
@@ -74,7 +66,6 @@ pub mod new_flow {
         Udp(super::UdpFlow),
     }
 }
-#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct TcpFlow {
     #[prost(message, optional, tag = "1")]
@@ -82,7 +73,6 @@ pub struct TcpFlow {
     #[prost(message, optional, tag = "2")]
     pub tunnel_info: ::core::option::Option<TunnelInfo>,
 }
-#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct UdpFlow {
     #[prost(message, optional, tag = "1")]
@@ -90,7 +80,6 @@ pub struct UdpFlow {
     #[prost(message, optional, tag = "3")]
     pub tunnel_info: ::core::option::Option<TunnelInfo>,
 }
-#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct UdpPacket {
     #[prost(bytes = "vec", tag = "1")]
@@ -98,7 +87,6 @@ pub struct UdpPacket {
     #[prost(message, optional, tag = "2")]
     pub remote_address: ::core::option::Option<Address>,
 }
-#[allow(clippy::derive_partial_eq_without_eq)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Address {
     #[prost(string, tag = "1")]
