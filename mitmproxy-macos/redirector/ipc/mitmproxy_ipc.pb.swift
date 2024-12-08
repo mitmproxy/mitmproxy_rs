df9bf27129571d32c40e68ff9229e2b301983232
Maximilian Hils <git@maximilianhils.com>
2024-12-09 00:21:54 +0100

    ipc: remove intercept conf default field

--- mitmproxy-macos/redirector/ipc/mitmproxy_ipc.pb.swift
+++ mitmproxy-macos/redirector/ipc/mitmproxy_ipc.pb.swift
@@ -122,8 +122,6 @@ struct MitmproxyIpc_InterceptConf: Sendable {
   // `Message` and `Message+*Additions` files in the SwiftProtobuf library for
   // methods supported on all messages.
 
-  var `default`: Bool = false
-
   var actions: [String] = []
 
   var unknownFields = SwiftProtobuf.UnknownStorage()
@@ -458,8 +456,7 @@ extension MitmproxyIpc_Packet: SwiftProtobuf.Message, SwiftProtobuf._MessageImpl
 extension MitmproxyIpc_InterceptConf: SwiftProtobuf.Message, SwiftProtobuf._MessageImplementationBase, SwiftProtobuf._ProtoNameProviding {
   static let protoMessageName: String = _protobuf_package + ".InterceptConf"
   static let _protobuf_nameMap: SwiftProtobuf._NameMap = [
-    1: .same(proto: "default"),
-    2: .same(proto: "actions"),
+    1: .same(proto: "actions"),
   ]
 
   mutating func decodeMessage<D: SwiftProtobuf.Decoder>(decoder: inout D) throws {
@@ -468,25 +465,20 @@ extension MitmproxyIpc_InterceptConf: SwiftProtobuf.Message, SwiftProtobuf._Mess
       // allocates stack space for every case branch when no optimizations are
       // enabled. https://github.com/apple/swift-protobuf/issues/1034
       switch fieldNumber {
-      case 1: try { try decoder.decodeSingularBoolField(value: &self.`default`) }()
-      case 2: try { try decoder.decodeRepeatedStringField(value: &self.actions) }()
+      case 1: try { try decoder.decodeRepeatedStringField(value: &self.actions) }()
       default: break
       }
     }
   }
 
   func traverse<V: SwiftProtobuf.Visitor>(visitor: inout V) throws {
-    if self.`default` != false {
-      try visitor.visitSingularBoolField(value: self.`default`, fieldNumber: 1)
-    }
     if !self.actions.isEmpty {
-      try visitor.visitRepeatedStringField(value: self.actions, fieldNumber: 2)
+      try visitor.visitRepeatedStringField(value: self.actions, fieldNumber: 1)
     }
     try unknownFields.traverse(visitor: &visitor)
   }
 
   static func ==(lhs: MitmproxyIpc_InterceptConf, rhs: MitmproxyIpc_InterceptConf) -> Bool {
-    if lhs.`default` != rhs.`default` {return false}
     if lhs.actions != rhs.actions {return false}
     if lhs.unknownFields != rhs.unknownFields {return false}
     return true
