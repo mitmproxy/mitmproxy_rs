71ce8d2862cad419f60af6b15c00c903f36b7c68
errorxyz <gjain1563@gmail.com>
2024-12-08 21:10:42 +0530

    Only refresh process specific information

--- src/processes/linux_list.rs
+++ src/processes/linux_list.rs
@@ -3,12 +3,16 @@ use anyhow::Result;
 use std::collections::hash_map::Entry;
 use std::collections::HashMap;
 use std::path::PathBuf;
-use sysinfo::System;
+use sysinfo::{ProcessesToUpdate, ProcessRefreshKind, System};
 
 pub fn active_executables() -> Result<ProcessList> {
     let mut executables: HashMap<PathBuf, ProcessInfo> = HashMap::new();
     let mut sys = System::new_all();
-    sys.refresh_all();
+    sys.refresh_processes_specifics(
+        ProcessesToUpdate::All,
+        true,
+        ProcessRefreshKind::everything(),
+    );
 
     for process in sys.processes().values() {
         // process.exe() will return empty path if there was an error while trying to read /proc/<pid>/exe.
