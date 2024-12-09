7d401508d028824d12f5a8e9ffe09f49004b5540
errorxyz <gjain1563@gmail.com>
2024-12-09 12:33:05 +0530

    fix

--- mitmproxy-rs/src/process_info.rs
+++ mitmproxy-rs/src/process_info.rs
@@ -3,6 +3,7 @@ use std::path::{Path, PathBuf};
 use anyhow::Result;
 use pyo3::prelude::*;
 
+#[cfg(any(windows, target_os = "macos", target_os = "linux"))]
 use mitmproxy::processes;
 
 #[pyclass(module = "mitmproxy_rs.process_info", frozen)]
