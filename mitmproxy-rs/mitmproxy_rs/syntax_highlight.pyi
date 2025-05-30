b66e181453d7bb3209063ac3f57ad9b1410f325c
Maximilian Hils <git@maximilianhils.com>
2025-05-30 18:32:42 +0200

    add type annotations

--- mitmproxy-rs/mitmproxy_rs/syntax_highlight.pyi
+++ mitmproxy-rs/mitmproxy_rs/syntax_highlight.pyi
@@ -6,10 +6,14 @@ from typing import Literal
 def highlight(text: str, language: Literal["xml", "yaml", "error", "none"]) -> list[tuple[str, str]]:
     pass
 
+def languages() -> list[str]:
+    pass
+
 def tags() -> list[str]:
     pass
 
 __all__ = [
     "highlight",
+    "languages",
     "tags",
 ]
